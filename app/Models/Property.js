207a7790f7e40abdeedacfd02b73e7a78eb108ff
Lakshamana <guitrompa1@gmail.com>
2020-04-07 10:21:31 -0300

    hidden fields + nearby scope

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -1,7 +1,9 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+/** @typedef {import('@adonisjs/lucid/src/Lucid/QueryBuilder')} QueryBuilder */
+
 const Model = use('Model')
+const Database = use('Database')
 
 class Property extends Model {
   images () {
@@ -10,6 +12,30 @@ class Property extends Model {
 
   user () {
     return this.belongsTo('App/Models/User')
+  }
+
+  static get hidden() {
+    return ['created_at', 'updated_at']
+  }
+
+  /**
+   *
+   * @param {QueryBuilder} query
+   * @param {*} lat
+   * @param {*} long
+   * @param {*} dist
+   */
+  static scopeNearby(query, lat, long, dist) {
+    const haversine =
+      `(6371 * acos(cos(radians(${lat}))
+      * cos(radians(latitude))
+      * cos(radians(longitude)
+      - radians(${long}))
+      + sin(radians(${lat}))
+      * sin(radians(latitude))))`
+    return query
+      .select('*', Database.raw(`${haversine} as distance`))
+      .whereRaw(`${haversine} < ${dist}`)
   }
 }
 
